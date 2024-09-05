05a2c5710f85b7e13a5a512581fd301bd06dc022
Alice Rose <hello@alicerosemurphy.com>
2024-09-05 03:15:57 +0300

    temporarily use fork branch for gulp-plugin-extras fix commands add husky pre-push hook update ci actions

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import File from 'vinyl';
-
-declare module 'gulp-plugin-extras' {
-	export function gulpPlugin(
-		name: string,
-		onFile: (file: File) => File,
-		options?: Options,
-	): Buffer | NodeJS.ReadableStream | null;
-}
